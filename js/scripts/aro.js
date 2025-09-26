bfce05404f98993a0d994dc2b6e8f8889a9ca412
AlekdSANS <matsafey2007@gmail.com>
2025-09-26 20:36:21 +0200

    html+js-update-0.91

---
+++ js/scripts/aro.js
@@ -0,0 +1,22 @@
+$(function(){
+  const aro = $(".main__footer-section");
+
+  aro.css({ 
+    top: "700px",
+    opacity: 0,
+    "pointer-events": "none"
+  });
+
+  $("#aro").on("click", function(){
+    if (aro.css("opacity") == 0){
+      aro.css("pointer-events", "auto").animate(
+        {top: "570px", opacity: 1}, 700);
+    }else{
+      aro.animate(
+        {top: "700px", opacity: 0}, 700, function(){
+          aro.css("pointer-events", "none");
+        }
+      );
+    }
+  });
+});
